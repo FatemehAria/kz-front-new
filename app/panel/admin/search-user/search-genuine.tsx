a878d4853e209ee19f87c2e4098ac4269a18f853
FatemehAria <ftemewh@gmail.com>
2024-03-07 15:03:42 +0330

    search and view for admin done

--- app/panel/admin/search-user/search-genuine.tsx
+++ app/panel/admin/search-user/search-genuine.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import React from "react";
 import vieweye from "../../../../public/ViewUsers/vieweye.svg";
+import search from "../../../../public/ViewUsers/search.svg";
 const SearchGenuineData = [
   {
     id: 1,
@@ -23,31 +24,45 @@ const SearchGenuineData = [
 ];
 function SearchGenuine() {
   return (
-    <div className="flex flex-col gap-5">
-      <div className="grid grid-cols-6 text-center">
-        <p>ردیف</p>
-        <p>نام سازمان</p>
-        <p>شماره ملی</p>
-        <p>شماره موبایل </p>
-        <p>شماره ثبت</p>
-        <p>مشاهده</p>
+    <div>
+      <div className="relative mb-3">
+        <input
+          type="text"
+          placeholder="جستجو بر اساس شماره موبایل"
+          className="w-full outline-none border border-[#4866CF] rounded-[8px] p-2"
+        />
+        <Image
+          src={search}
+          alt="search"
+          className="absolute left-2 top-1/2 -translate-y-1/2"
+        />
       </div>
-      {SearchGenuineData.map((item) => (
-        <div
-          key={item.id}
-          className="grid grid-cols-6 text-center py-1 bg-[#EAEFF6] rounded-[4px] cursor-pointer"
-          //   onClick={() => setStep(2)}
-        >
-          <p>{item.title}</p>
-          <p>{item.title1}</p>
-          <p>{item.title2}</p>
-          <p>{item.title3}</p>
-          <p>{item.title4}</p>
-          <div className="flex justify-center">
-            <Image src={item.imgSrc} alt="مشاهده" width={20} height={20} />
-          </div>
+      <div className="flex flex-col gap-5">
+        <div className="grid grid-cols-6 text-center">
+          <p>ردیف</p>
+          <p>نام سازمان</p>
+          <p>شماره ملی</p>
+          <p>شماره موبایل </p>
+          <p>شماره ثبت</p>
+          <p>مشاهده</p>
         </div>
-      ))}
+        {SearchGenuineData.map((item) => (
+          <div
+            key={item.id}
+            className="grid grid-cols-6 text-center py-1 bg-[#EAEFF6] rounded-[4px] cursor-pointer"
+            //   onClick={() => setStep(2)}
+          >
+            <p>{item.title}</p>
+            <p>{item.title1}</p>
+            <p>{item.title2}</p>
+            <p>{item.title3}</p>
+            <p>{item.title4}</p>
+            <div className="flex justify-center">
+              <Image src={item.imgSrc} alt="مشاهده" width={20} height={20} />
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
--- app/panel/admin/search-user/search-legal.tsx
+++ app/panel/admin/search-user/search-legal.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import React from "react";
 import vieweye from "../../../../public/ViewUsers/vieweye.svg";
+import search from "../../../../public/ViewUsers/search.svg";
 const SearchLegalData = [
   {
     id: 1,
@@ -22,29 +23,39 @@ const SearchLegalData = [
 
 function SearchLegal() {
   return (
-    <div className="flex flex-col gap-5">
-      <div className="grid grid-cols-5 text-center">
-        <p>ردیف</p>
-        <p>شماره موبایل</p>
-        <p>نام و نام خانوادگی</p>
-        <p>ایمیل</p>
-        <p>مشاهده</p>
+    <div>
+      <div className="relative mb-3">
+        <input
+          type="text"
+          placeholder="جستجو بر اساس شماره موبایل"
+          className="w-full outline-none border border-[#4866CF] rounded-[8px] p-2"
+        />
+        <Image src={search} alt="search" className="absolute left-2 top-1/2 -translate-y-1/2"/>
       </div>
-      {SearchLegalData.map((item) => (
-        <div
-          key={item.id}
-          className="grid grid-cols-5 text-center py-1 bg-[#EAEFF6] rounded-[4px] cursor-pointer"
-          //   onClick={() => setStep(2)}
-        >
-          <p>{item.title}</p>
-          <p>{item.title1}</p>
-          <p>{item.title2}</p>
-          <p>{item.title3}</p>
-          <div className="flex justify-center">
-            <Image src={item.imgSrc} alt="مشاهده" width={20} height={20} />
-          </div>
+      <div className="flex flex-col gap-5">
+        <div className="grid grid-cols-5 text-center">
+          <p>ردیف</p>
+          <p>شماره موبایل</p>
+          <p>نام و نام خانوادگی</p>
+          <p>ایمیل</p>
+          <p>مشاهده</p>
         </div>
-      ))}
+        {SearchLegalData.map((item) => (
+          <div
+            key={item.id}
+            className="grid grid-cols-5 text-center py-1 bg-[#EAEFF6] rounded-[4px] cursor-pointer"
+            //   onClick={() => setStep(2)}
+          >
+            <p>{item.title}</p>
+            <p>{item.title1}</p>
+            <p>{item.title2}</p>
+            <p>{item.title3}</p>
+            <div className="flex justify-center">
+              <Image src={item.imgSrc} alt="مشاهده" width={20} height={20} />
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
