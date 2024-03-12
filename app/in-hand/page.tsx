7447a4c99ca954abe3c34bdfe90a647bb1a56cc3
FatemehAria <ftemewh@gmail.com>
2024-03-12 18:05:08 +0330

    in-hand almost done,needs more styling

--- app/in-hand/page.tsx
+++ app/in-hand/page.tsx
@@ -4,13 +4,24 @@ import styles from "./in-hand.module.css";
 import { animated, useSpring } from "@react-spring/web";
 import Image from "next/image";
 import board from "../../public/in-hand/board.svg";
+import { useStopwatch } from "react-timer-hook";
 const { inHandBackground } = styles;
 
 const InHand = () => {
   const [isAnimating, setIsAnimating] = useState(false);
   const [heightReached, setHeightReached] = useState(false);
   const [timeLeft, setTimeLeft] = useState(30);
-
+  const {
+    totalSeconds,
+    seconds,
+    minutes,
+    hours,
+    days,
+    isRunning,
+    start,
+    pause,
+    reset,
+  } = useStopwatch({ autoStart: true });
   const stopAnimation = () => {
     setIsAnimating(false);
   };
@@ -34,7 +45,7 @@ const InHand = () => {
     onStart: () => {
       setIsAnimating(true);
     },
-    onFrame: (props) => {
+    onFrame: (props: any) => {
       if (props.y >= 100 && !heightReached) {
         setHeightReached(true);
         startTimer();
@@ -84,11 +95,72 @@ const InHand = () => {
               top: hookSpring.y.to((value) => `${value}px`),
               left: "50%",
               transform: "translateX(-50%)",
-              width: "230px",
-              height: "230px",
+              width: "250px",
+              height: "250px",
             }}
           >
-            <Image src={board} alt="board" />
+            <div className="relative">
+              <Image src={board} alt="board" />
+              <span className="font-bold absolute top-[8rem] left-8 text-[20px]">
+                به زودی حاضر میشه
+              </span>
+              <div className="absolute top-[10rem] left-1/2 -translate-x-1/2">
+                <div className="text-[25px] font-bold flex">
+                  <span className="px-1 rounded-sm text-yellow-600 flex gap-1">
+                    {String(days)
+                      .split("")
+                      .map((digit, index) => (
+                        <span
+                          key={index}
+                          className="border border-black px-2 rounded-[4px]"
+                        >
+                          {digit}
+                        </span>
+                      ))}
+                  </span>
+                  :
+                  <span className="px-1 rounded-sm text-yellow-600 flex gap-1">
+                    {String(hours)
+                      .split("")
+                      .map((digit, index) => (
+                        <span
+                          key={index}
+                          className="border border-black px-2 rounded-[4px]"
+                        >
+                          {digit}
+                        </span>
+                      ))}
+                  </span>
+                  :
+                  <span className="px-1 rounded-sm text-yellow-600 flex gap-1">
+                    {String(minutes)
+                      .split("")
+                      .map((digit, index) => (
+                        <span
+                          key={index}
+                          className="border border-black px-2 rounded-[4px]"
+                        >
+                          {digit}
+                        </span>
+                      ))}
+                  </span>
+                  :
+                  <span className="px-1 rounded-sm text-yellow-600 flex gap-1">
+                    {String(seconds)
+                      .split("")
+                      .map((digit, index) => (
+                        <span
+                          key={index}
+                          className="border border-black px-2 rounded-[4px] relative"
+                        >
+                          {digit}
+                          {/* <span className="absolute h-[1px] bg-black w-[3rem] bottom-5 left-0"></span> */}
+                        </span>
+                      ))}
+                  </span>
+                </div>
+              </div>
+            </div>
           </animated.div>
           <animated.div
             style={{
