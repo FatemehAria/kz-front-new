0f50a950ac3e2e23e9f76445e1c712fe31ee5257
FatemehAria <ftemewh@gmail.com>
2024-03-13 10:56:00 +0330

    admin support updated

--- app/panel/admin/support/page.tsx
+++ app/panel/admin/support/page.tsx
@@ -11,6 +11,9 @@ import {
   getTokenFromLocal,
 } from "@/redux/features/user/userSlice";
 import checkmark from "../../../../public/Panel/checkmark.svg";
+import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
+import vieweye from "../../../../public/ViewUsers/vieweye.svg";
+import CloseTicketModal from "./components/close-ticket-modal";
 const Support = () => {
   const dispatch = useDispatch();
   const { localUserId, localToken } = useSelector(
@@ -22,8 +25,15 @@ const Support = () => {
     dispatch<any>(fetchUserProfile());
   }, []);
   const [allTickets, setAllTickets] = useState([]);
+  const [allTicketsStatus, setAllTicketsStatus] = useState({
+    error: "",
+    loading: false,
+  });
+  const [showModal, setShowModal] = useState(false);
+  const [closeTicketId, setCloseTicketId] = useState("");
   const getAllTickets = async () => {
     try {
+      setAllTicketsStatus((last) => ({ ...last, loading: true }));
       const { data } = await axios(
         `https://keykavoos.liara.run/Admin/AllTickets/${localUserId}`,
         {
@@ -33,8 +43,10 @@ const Support = () => {
         }
       );
       setAllTickets(data.data);
+      setAllTicketsStatus((last) => ({ ...last, loading: false }));
       console.log(data);
     } catch (error) {
+      setAllTicketsStatus({ error: "خطا در خواندن اطلاعات", loading: false });
       console.log(error);
     }
   };
@@ -45,6 +57,15 @@ const Support = () => {
   }, [localUserId]);
   return (
     <div className="flex flex-col gap-3">
+      {showModal && (
+        <CloseTicketModal
+          showModal={showModal}
+          setShowModal={setShowModal}
+          text={`آیا از بستن تیکت مطمئنید؟`}
+          setAllTickets={setAllTickets}
+          closeTicketId={closeTicketId}
+        />
+      )}
       <Link
         href="/panel/admin/support/add-new-placard"
         className="flex flex-row gap-2 bg-[#4866CE] text-white p-2 rounded-[4px] w-[120px]"
@@ -61,22 +82,39 @@ const Support = () => {
             <p>تاریخ بروزرسانی</p>
             <p>عملیات</p>
           </div>
-          {allTickets.map((item: any, index) => (
-            <Link
-              key={item._id}
-              className="grid grid-cols-5 text-center py-1 bg-[#EAEFF6] rounded-[4px] cursor-pointer"
-              href={`/panel/admin/support/ticket-detail?id=${item._id}`}
-            >
-              <p>{index + 1}</p>
-              <p>{item.Title}</p>
-              <p>{item.Blocked ? "بسته شده" : "بسته نشده"}</p>
-              <p>*</p>
-              <div className="flex flex-row justify-center gap-2">
-                <Image src={checkmark} alt="" width={20} />
-                <span>بستن</span>
+          {allTicketsStatus.loading ? (
+            <SkeletonTheme>
+              <Skeleton count={1} className="p-2" baseColor="#EAEFF6" />
+            </SkeletonTheme>
+          ) : (
+            allTickets.map((item: any, index) => (
+              <div
+                key={item._id}
+                className="grid grid-cols-5 text-center py-1 bg-[#EAEFF6] rounded-[4px] cursor-pointer"
+              >
+                <p>{index + 1}</p>
+                <p>{item.Title}</p>
+                <p>{item.Blocked ? "بسته شده" : "بسته نشده"}</p>
+                <p>*</p>
+                <div className="flex flex-row justify-center gap-2">
+                  <Image
+                    src={checkmark}
+                    alt="بستن"
+                    width={20}
+                    onClick={() => (
+                      setShowModal(true), setCloseTicketId(item._id)
+                    )}
+                    className="cursor-pointer"
+                  />
+                  <Link
+                    href={`/panel/admin/support/ticket-detail?id=${item._id}`}
+                  >
+                    <Image src={vieweye} alt="مشاهده" width={20} />
+                  </Link>
+                </div>
               </div>
-            </Link>
-          ))}
+            ))
+          )}
         </div>
       </div>
     </div>
--- app/panel/admin/support/ticket-detail/page.tsx
+++ app/panel/admin/support/ticket-detail/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import React, { useEffect, useState } from "react";
 import TicketInfoField from "@/app/panel/support/ticket-detail/components/ticket-info-filed";
-import { useSearchParams } from "next/navigation";
+import { useRouter, useSearchParams } from "next/navigation";
 import { useDispatch, useSelector } from "react-redux";
 import {
   fetchUserProfile,
@@ -9,7 +9,9 @@ import {
   getTokenFromLocal,
 } from "@/redux/features/user/userSlice";
 import axios from "axios";
-
+import { IoArrowBack } from "react-icons/io5";
+import Chat from "./components/chat";
+const moment = require("moment-jalaali");
 function TicketDetail() {
   const [ticketDetail, setTicketDetail] = useState({
     Title: "",
@@ -17,21 +19,28 @@ function TicketDetail() {
     RelativeUnit: "",
     SentDate: "",
     RespondDate: "",
+    SenderText: [],
   });
   const { localUserId, localToken } = useSelector(
     (state: any) => state.userData
   );
   const params = useSearchParams();
   const id = params.get("id");
+  const router = useRouter();
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(getIdFromLocal());
     dispatch(getTokenFromLocal());
     dispatch<any>(fetchUserProfile());
   }, []);
-
+  const [ticketDetailStatus, setTicketDetailStatus] = useState({
+    error: "",
+    loading: false,
+  });
+  const [textInput, setTextInput] = useState("");
   const getTicketDetail = async () => {
     try {
+      setTicketDetailStatus((last) => ({ ...last, loading: true }));
       const { data } = await axios(
         `https://keykavoos.liara.run/Admin/OneTicket/${localUserId}/${id}`,
         {
@@ -41,53 +50,113 @@ function TicketDetail() {
         }
       );
       setTicketDetail({
-        Title:data.data.Title,
-        RelativeUnit:data.data.RelevantUnit,
-        RespondDate:"",
-        Responsor:"",
-        SentDate:""
-      })
+        Title: data.data.Title,
+        RelativeUnit: data.data.RelevantUnit,
+        RespondDate: "",
+        Responsor: "",
+        SentDate: moment(
+          data.data.createdAt,
+          "YYYY-MM-DDTHH:mm:ss.SSSZ"
+        ).format("jYYYY/jM/jD"),
+        SenderText: data.data.text,
+      });
+      setTicketDetailStatus((last) => ({ ...last, loading: false }));
+      console.log(data);
+    } catch (error) {
+      setTicketDetailStatus({ error: "خطا در خواندن اطلاعات", loading: false });
+      console.log(error);
+    }
+  };
+  const updateSenderBox = (newText: string) => {
+    const updatedSenderText = [
+      ...ticketDetail.SenderText,
+      { content: newText },
+    ];
+    return updatedSenderText;
+  };
+  const sendResponseTicket = async (textInput: string) => {
+    try {
+      const { data } = await axios.post(
+        `https://keykavoos.liara.run/Admin/ResponseTicket/${localUserId}/${id}`,
+        {
+          text: textInput,
+        },
+        {
+          headers: {
+            authorization: `Bearer ${localToken}`,
+          },
+        }
+      );
+      const updatedSenderText = updateSenderBox(textInput);
+      setTicketDetail((prevTicketDetail: any) => ({
+        ...prevTicketDetail,
+        SenderText: updatedSenderText,
+      }));
       console.log(data);
     } catch (error) {
       console.log(error);
     }
   };
-
   useEffect(() => {
     if (localUserId) {
       getTicketDetail();
     }
   }, [localUserId]);
   return (
-    <div className="bg-white shadow mx-auto rounded-2xl py-[3%] px-[3%] w-full">
-      <div className="grid grid-cols-2 gap-[5%]">
-        <TicketInfoField label="عنوان تیکت:" text={ticketDetail.Title} />
-        <TicketInfoField
-          label="مسئول پاسخگویی:"
-          text={ticketDetail.Responsor}
-        />
-        <TicketInfoField
-          label="واحد مربوطه تیکت:"
-          text={ticketDetail.RelativeUnit}
-        />
-        <TicketInfoField
-          label="تاریخ ارسال تیکت:"
-          text={ticketDetail.SentDate}
-        />
-        <TicketInfoField
-          label="تاریخ پاسخگویی:"
-          text={ticketDetail.RespondDate}
+    <div>
+      <div
+        className="flex justify-end w-full text-xl cursor-pointer absolute -top-12"
+        onClick={() => router.back()}
+      >
+        <div className="rounded-full p-2 bg-white">
+          <IoArrowBack />
+        </div>
+      </div>
+
+      <div className="bg-white shadow mx-auto rounded-2xl py-[3%] px-[3%] w-full">
+        <div className="grid grid-cols-2 gap-[5%]">
+          <TicketInfoField
+            label="عنوان تیکت:"
+            text={ticketDetail.Title}
+            ticketDetailStatus={ticketDetailStatus.loading}
+          />
+          <TicketInfoField
+            label="مسئول پاسخگویی:"
+            text={ticketDetail.Responsor}
+            ticketDetailStatus={ticketDetailStatus.loading}
+          />
+          <TicketInfoField
+            label="واحد مربوطه تیکت:"
+            text={ticketDetail.RelativeUnit}
+            ticketDetailStatus={ticketDetailStatus.loading}
+          />
+          <TicketInfoField
+            label="تاریخ ارسال تیکت:"
+            text={ticketDetail.SentDate}
+            ticketDetailStatus={ticketDetailStatus.loading}
+          />
+          <TicketInfoField
+            label="تاریخ پاسخگویی:"
+            text={ticketDetail.RespondDate}
+            ticketDetailStatus={ticketDetailStatus.loading}
+          />
+        </div>
+        <div
+          style={{
+            border: "none",
+            borderTop: "3px solid",
+            borderImage:
+              "linear-gradient(to right, #FFFFFF 0%, #4866CE 45% ,#4866CE 55% , #FFFFFF 100%) 1",
+            margin: "5% 0",
+          }}
+        ></div>
+        <Chat
+          senderText={ticketDetail.SenderText}
+          textInput={textInput}
+          setTextInput={setTextInput}
+          sendResponseTicket={sendResponseTicket}
         />
       </div>
-      <div
-        style={{
-          border: "none",
-          borderTop: "3px solid",
-          borderImage:
-            "linear-gradient(to right, #FFFFFF 0%, #4866CE 45% ,#4866CE 55% , #FFFFFF 100%) 1",
-          margin: "5% 0",
-        }}
-      ></div>
     </div>
   );
 }
