d66935c8961d83c20fad05ff74ab636b8e2e88c1
FatemehAria <ftemewh@gmail.com>
2024-04-20 15:36:01 +0330

    counter for statistics set uing react-countup

--- components/home-components/intro-statistics-atom.tsx
+++ components/home-components/intro-statistics-atom.tsx
@@ -1,7 +1,10 @@
+"use client";
 import Image, { StaticImageData } from "next/image";
 import React from "react";
+import CountUp from "react-countup";
+
 type IntroStatisticsAtomProps = {
-  amount: string;
+  amount: number;
   jobDone: string;
   imgSrc: StaticImageData;
 };
@@ -13,10 +16,16 @@ function IntroStatisticsAtom({
   return (
     <div className="flex flex-row text-center items-center gap-2">
       <div className="flex flex-col">
-        <p className="font-faNum font-semibold sm:text-[30px]">{amount}</p>
+        <div className="flex flex-row items-center justify-around font-semibold sm:text-[30px]">
+          <CountUp
+            end={amount}
+            className="font-faNum"
+            duration={5}
+          />
+        </div>
         <p className="font-normal sm:text-[16px] text-[10px]">{jobDone}</p>
       </div>
-      <div className="bg-[#D0DBEC] rounded-full w-[70px] h-[70px] flex justify-center items-center" >
+      <div className="bg-[#D0DBEC] rounded-full w-[70px] h-[70px] flex justify-center items-center">
         <Image src={imgSrc} alt={jobDone} />
       </div>
     </div>
--- components/home-components/intro-statistics.tsx
+++ components/home-components/intro-statistics.tsx
@@ -2,19 +2,16 @@ import React from "react";
 import company from "../../public/Intro/company.svg";
 import agreement from "../../public/Intro/agreement.svg";
 import IntroStatisticsAtom from "./intro-statistics-atom";
+
 function IntroStatistics() {
   return (
     <div
       className="flex flex-row items-center gap-5 justify-center text-[#68707A] mt-[3%]"
       dir="rtl"
     >
+      <IntroStatisticsAtom amount={68000} imgSrc={company} jobDone="شرکت همکار" />
       <IntroStatisticsAtom
-        amount="68 هزار"
-        imgSrc={company}
-        jobDone="شرکت همکار"
-      />
-      <IntroStatisticsAtom
-        amount="12 هزار"
+        amount={12000}
         imgSrc={agreement}
         jobDone="سایت طراحی شده"
       />
